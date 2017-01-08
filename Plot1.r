7a1b3672438eb5b0f0e1a98b4f4fe3116add7d80
smy73 <soumyamukherjee@outlook.com>
2017-01-08 22:38:00 +0530

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -1,9 +1,9 @@
-## Getting full dataset
+## Reading full dataset
 data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
-## Subsetting the data
+## Subsetting and extracting only relevant data
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
 
